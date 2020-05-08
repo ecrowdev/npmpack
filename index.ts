e61e11e1a7c7c618a7a39c60b950dd65fad15e6b
Eric Crowell <crowelle@wudn-crowelle.udev.six3>
2020-05-08 15:59:23 -0600

    feat(lint): added linting

--- index.ts
+++ index.ts
@@ -12,70 +12,70 @@ export type TObject = { [key: string]: any };
 /**
  * NPM Pack options.
  */
-export interface IOptions {
-  /**
-   * Root directory containing the package files
-   */
-  root?: string;
-  /**
-   * Individual files or directories to copy
-   */
-  copy?: string[];
-  /**
-   * Glob pattern(s) of files to include
-   */
-  include?: string[];
-  /**
-   * Glob pattern(s) of files to exclude
-   */
-  exclude?: string[];
-  /**
-   * Path to output matched files
-   */
-  output?: string;
-  /**
-   * JSON string or JSON Object to override properties in the package.json
-   */
-  packagejson?: string | TObject;
+export interface Options {
+	/**
+	 * Root directory containing the package files
+	 */
+	root?: string;
+	/**
+	 * Individual files or directories to copy
+	 */
+	copy?: string[];
+	/**
+	 * Glob pattern(s) of files to include
+	 */
+	include?: string[];
+	/**
+	 * Glob pattern(s) of files to exclude
+	 */
+	exclude?: string[];
+	/**
+	 * Path to output matched files
+	 */
+	output?: string;
+	/**
+	 * JSON string or JSON Object to override properties in the package.json
+	 */
+	packagejson?: string | TObject;
 }
 
-export interface IStrictOptions {
-  /**
-   * Root directory containing the package files
-   */
-  root: string;
-  /**
-   * Individual files or directories to copy
-   */
-  copy: string[];
-  /**
-   * Glob pattern(s) of files to include
-   */
-  include: string[];
-  /**
-   * Glob pattern(s) of files to exclude
-   */
-  exclude: string[];
-  /**
-   * Path to output matched files
-   */
-  output: string;
-  /**
-   * JSON string or JSON Object to override properties in the package.json
-   */
-  packagejson: { [key: string]: any };
+export interface StrictOptions {
+	/**
+	 * Root directory containing the package files
+	 */
+	root: string;
+	/**
+	 * Individual files or directories to copy
+	 */
+	copy: string[];
+	/**
+	 * Glob pattern(s) of files to include
+	 */
+	include: string[];
+	/**
+	 * Glob pattern(s) of files to exclude
+	 */
+	exclude: string[];
+	/**
+	 * Path to output matched files
+	 */
+	output: string;
+	/**
+	 * JSON string or JSON Object to override properties in the package.json
+	 */
+	packagejson: { [key: string]: any };
 }
 
 /**
  * Default options
  */
-export const defaultOptions: IOptions = {
-  root: '.',
-  copy: [],
-  include: [],
-  exclude: [],
-  output: 'pkg',
-  packagejson: {},
+export const defaultOptions: Options = {
+	root: '.',
+	copy: [],
+	include: [],
+	exclude: [],
+	output: 'pkg',
+	packagejson: {},
 };
 
 /**
@@ -85,48 +85,48 @@ export const defaultOptions: IOptions = {
  * @param noWarn Do not output warnings.
  */
 export function getFilesGlob(
-  includePatterns: string[],
-  excludePatterns: string[],
-  noWarn: boolean = false
-) {
-  let returnFiles: string[] = [];
-  includePatterns.forEach((pattern) => {
-    const files = glob.sync(pattern, {
-      nodir: true,
-      ignore: excludePatterns,
-      absolute: false,
-    });
-    if (!files.length) {
-      !noWarn &&
-        console.warn(`Warning: No files found for the pattern "${pattern}"`);
-      return;
-    }
-    returnFiles = returnFiles.concat(files);
-  });
-  return returnFiles;
+	includePatterns: string[],
+	excludePatterns: string[],
+	noWarn = false
+): string[] {
+	let returnFiles: string[] = [];
+	includePatterns.forEach((pattern) => {
+		const files = glob.sync(pattern, {
+			nodir: true,
+			ignore: excludePatterns,
+			absolute: false,
+		});
+		if (!files.length) {
+			!noWarn &&
+				console.warn(`Warning: No files found for the pattern "${pattern}"`);
+			return;
+		}
+		returnFiles = returnFiles.concat(files);
+	});
+	return returnFiles;
 }
 
 /**
  * Creates new directories if they don't exist.
  * @param path File path
  */
-export function createDirectory(path: string) {
-  if (fs.existsSync(path)) {
-    return;
-  }
-  fs.mkdirSync(path, { recursive: true });
+export function createDirectory(path: string): void {
+	if (fs.existsSync(path)) {
+		return;
+	}
+	fs.mkdirSync(path, { recursive: true });
 }
 
 /**
  * Fancy parse JSON string.
  * @param path File path
  */
-export function parseJSON(json: string | TObject) {
-  let returnJson = json;
-  if (typeof json === 'string') {
-    returnJson = JSON.parse(json);
-  }
-  return returnJson as TObject;
+export function parseJSON(json: string | TObject): TObject {
+	let returnJson = json;
+	if (typeof json === 'string') {
+		returnJson = JSON.parse(json);
+	}
+	return returnJson as TObject;
 }
 
 /**
@@ -134,94 +134,94 @@ export function parseJSON(json: string | TObject) {
  * @param filesDirs The array of files and directories.
  * @param path The path to copy the files and directories to.
  */
-export function copyFilesDirs(filesDirs: string[], path: string) {
-  for (let i = 0; i < filesDirs.length; i++) {
-    const fileDir = filesDirs[i];
+export function copyFilesDirs(filesDirs: string[], path: string): void {
+	for (let i = 0; i < filesDirs.length; i++) {
+		const fileDir = filesDirs[i];
 
-    // Ensure that the file/directory exists.
-    if (!fs.existsSync(fileDir)) {
-      console.warn(`WARN: ${fileDir} does not exist.`);
-      continue;
-    }
+		// Ensure that the file/directory exists.
+		if (!fs.existsSync(fileDir)) {
+			console.warn(`WARN: ${fileDir} does not exist.`);
+			continue;
+		}
 
-    const fileDirStats = fs.lstatSync(fileDir);
+		const fileDirStats = fs.lstatSync(fileDir);
 
-    if (fileDirStats.isFile() || fileDirStats.isDirectory()) {
-      // Copy the template file to the new path with the new name.
-      // fs.copyFileSync(`${PATH_TEMPLATES}/${file}`, fullFilePath);
-      console.log('FILE!: ', fileDir);
-      fs.copySync(fileDir, `${path}/${p.basename(fileDir)}`);
-    }
-  }
+		if (fileDirStats.isFile() || fileDirStats.isDirectory()) {
+			// Copy the template file to the new path with the new name.
+			// fs.copyFileSync(`${PATH_TEMPLATES}/${file}`, fullFilePath);
+			console.log('FILE!: ', fileDir);
+			fs.copySync(fileDir, `${path}/${p.basename(fileDir)}`);
+		}
+	}
 }
 
 /**
  * The main pack execution function
  */
-export function execute(options: IOptions = {}) {
-  /**
-   * Set default properties on any configuration that wasn't set.
-   */
-  const config = {
-    ...defaultOptions,
-    ...options,
-  } as IStrictOptions;
-  const { root, copy, include, exclude, output } = config;
-  const packagejson: TObject = parseJSON(config.packagejson);
+export function execute(options: Options = {}): void {
+	/**
+	 * Set default properties on any configuration that wasn't set.
+	 */
+	const config = {
+		...defaultOptions,
+		...options,
+	} as StrictOptions;
+	const { root, /**copy, include, exclude,*/ output } = config;
+	const packagejson: TObject = parseJSON(config.packagejson);
 
-  /**
-   * The array of all files and directories to copy.
-   */
-  let filesDirs: string[] = [];
+	/**
+	 * The array of all files and directories to copy.
+	 */
+	let filesDirs: string[] = [];
 
-  /**
-   * Prepare the output directory.
-   */
-  createDirectory(output);
+	/**
+	 * Prepare the output directory.
+	 */
+	createDirectory(output);
 
-  /**
-   * Read and write package.json file.
-   */
-  const packageJsonPath = `${root}/package.json`;
-  if (!fs.existsSync(packageJsonPath)) {
-    console.error(
-      `ERROR: package.json file does not exist in the root "${root}" folder.`
-    );
-    process.exit(1);
-  }
+	/**
+	 * Read and write package.json file.
+	 */
+	const packageJsonPath = `${root}/package.json`;
+	if (!fs.existsSync(packageJsonPath)) {
+		console.error(
+			`ERROR: package.json file does not exist in the root "${root}" folder.`
+		);
+		process.exit(1);
+	}
 
-  let packageJsonObject = {};
-  const packageJsonData = fs.readFileSync(packageJsonPath).toString();
-  try {
-    packageJsonObject = JSON.parse(packageJsonData);
-  } catch (error) {
-    console.error(`${error.name}: ${error.message}`);
-    process.exit(1);
-  }
+	let packageJsonObject = {};
+	const packageJsonData = fs.readFileSync(packageJsonPath).toString();
+	try {
+		packageJsonObject = JSON.parse(packageJsonData);
+	} catch (error) {
+		console.error(`${error.name}: ${error.message}`);
+		process.exit(1);
+	}
 
-  const newPackageJson = {
-    ...packageJsonObject,
-    ...packagejson,
-  };
+	const newPackageJson = {
+		...packageJsonObject,
+		...packagejson,
+	};
 
-  fs.writeFileSync(
-    `${output}/package.json`,
-    JSON.stringify(newPackageJson, null, 2)
-  );
+	fs.writeFileSync(
+		`${output}/package.json`,
+		JSON.stringify(newPackageJson, null, 2)
+	);
 
-  /**
-   * Find supportive files from the root directory.
-   */
-  filesDirs = filesDirs.concat(
-    getFilesGlob(
-      [`${root}/README*`, `${root}/LICENSE*`, `${root}/CHANGELOG*`],
-      [],
-      true
-    )
-  );
+	/**
+	 * Find supportive files from the root directory.
+	 */
+	filesDirs = filesDirs.concat(
+		getFilesGlob(
+			[`${root}/README*`, `${root}/LICENSE*`, `${root}/CHANGELOG*`],
+			[],
+			true
+		)
+	);
 
-  /**
-   * Finally, copy the files and directories to the package folder.
-   */
-  copyFilesDirs(filesDirs, output);
+	/**
+	 * Finally, copy the files and directories to the package folder.
+	 */
+	copyFilesDirs(filesDirs, output);
 }
--- npmpack.ts
+++ npmpack.ts
@@ -13,53 +13,53 @@ import packageJson from './package.json';
  * Yargs options
  */
 const yargsOptions: { [key: string]: yargs.Options } = {
-  root: {
-    alias: 'r',
-    string: true,
-    describe: 'Root directory containing the package files',
-    default: '.',
-  },
-  copy: {
-    alias: 'f',
-    array: true,
-    describe: 'Individual files or directories to copy',
-    default: [],
-  },
-  include: {
-    alias: 'i',
-    array: true,
-    describe: 'Glob pattern(s) of files to include',
-    default: ['src/**/*.js'],
-  },
-  exclude: {
-    alias: 'e',
-    array: true,
-    describe: 'Glob pattern(s) of files to exclude',
-    default: [],
-  },
-  output: {
-    alias: 'o',
-    string: true,
-    type: 'string',
-    describe: 'Path to output matched files',
-    default: 'pkg',
-  },
-  packagejson: {
-    alias: 'p',
-    string: true,
-    array: true,
-    describe: 'JSON string to override properties in the package.json',
-    default: {},
-  },
+	root: {
+		alias: 'r',
+		string: true,
+		describe: 'Root directory containing the package files',
+		default: '.',
+	},
+	copy: {
+		alias: 'f',
+		array: true,
+		describe: 'Individual files or directories to copy',
+		default: [],
+	},
+	include: {
+		alias: 'i',
+		array: true,
+		describe: 'Glob pattern(s) of files to include',
+		default: ['src/**/*.js'],
+	},
+	exclude: {
+		alias: 'e',
+		array: true,
+		describe: 'Glob pattern(s) of files to exclude',
+		default: [],
+	},
+	output: {
+		alias: 'o',
+		string: true,
+		type: 'string',
+		describe: 'Path to output matched files',
+		default: 'pkg',
+	},
+	packagejson: {
+		alias: 'p',
+		string: true,
+		array: true,
+		describe: 'JSON string to override properties in the package.json',
+		default: {},
+	},
 };
 
 /**
  * Command configuration.
  */
 const command = yargs
-  .options(yargsOptions)
-  .config()
-  .version(packageJson.version);
+	.options(yargsOptions)
+	.config()
+	.version(packageJson.version);
 
 /**
  * Get arguments as object.
@@ -70,54 +70,54 @@ const argv = command.argv;
  * Type checking
  * Mainly for the config file.
  */
-function typeCheck(args: { [key: string]: any }) {
-  const types: { [key: string]: string } = {
-    array: 'object',
-    boolean: 'boolean',
-    count: 'boolean',
-    number: 'number',
-    string: 'string',
-  };
-  for (let argKey in args) {
-    // Skip aliases since full option argKey will be the same.
-    if (argKey.length <= 1 || ['_', '$0'].includes(argKey)) {
-      continue;
-    }
+function typeCheck(args: { [key: string]: any }): void {
+	const types: { [key: string]: string } = {
+		array: 'object',
+		boolean: 'boolean',
+		count: 'boolean',
+		number: 'number',
+		string: 'string',
+	};
+	for (const argKey in args) {
+		// Skip aliases since full option argKey will be the same.
+		if (argKey.length <= 1 || ['_', '$0'].includes(argKey)) {
+			continue;
+		}
 
-    const arg: any = args[argKey];
-    const opts: yargs.Options = yargsOptions[argKey];
+		const arg: any = args[argKey];
+		const opts: yargs.Options = yargsOptions[argKey];
 
-    /**
-     * If opt is undefined, means an invalid option was passed in.
-     */
-    if (!opts) {
-      console.warn(`WARN: Unknown option "${argKey}" was passed in.`);
-      continue;
-    }
+		/**
+		 * If opt is undefined, means an invalid option was passed in.
+		 */
+		if (!opts) {
+			console.warn(`WARN: Unknown option "${argKey}" was passed in.`);
+			continue;
+		}
 
-    /**
-     * Type check the passed in value.
-     */
-    let foundTypes = [];
-    let valid = false;
-    for (let optKey in opts) {
-      if (types[optKey]) {
-        foundTypes.push(optKey);
-        if (typeof arg === types[optKey]) {
-          valid = true;
-          break;
-        }
-      }
-    }
+		/**
+		 * Type check the passed in value.
+		 */
+		const foundTypes = [];
+		let valid = false;
+		for (const optKey in opts) {
+			if (types[optKey]) {
+				foundTypes.push(optKey);
+				if (typeof arg === types[optKey]) {
+					valid = true;
+					break;
+				}
+			}
+		}
 
-    if (!valid) {
-      console.error(`
+		if (!valid) {
+			console.error(`
       ERROR: Option "${argKey}" (${arg}) is not the correct type.\n
       Expected [${foundTypes.join('|')}] but got [${typeof arg}].
       `);
-      process.exit(1);
-    }
-  }
+			process.exit(1);
+		}
+	}
 }
 
 /**
@@ -129,10 +129,10 @@ typeCheck(argv);
  * Execute NPMPack.
  */
 NPMPack.execute({
-  root: argv.root as string,
-  copy: argv.copy as string[],
-  include: argv.include as string[],
-  exclude: argv.exclude as string[],
-  output: argv.output as string,
-  packagejson: argv.packagejson as string | NPMPack.TObject,
+	root: argv.root as string,
+	copy: argv.copy as string[],
+	include: argv.include as string[],
+	exclude: argv.exclude as string[],
+	output: argv.output as string,
+	packagejson: argv.packagejson as string | NPMPack.TObject,
 });
